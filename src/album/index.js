ed3617812eb23282f4670de48e5a661859f97b55
Cristian Buelvas <crisrabumen@gmail.com>
2021-11-29 21:09:13 -0500

    Playlist y album

--- src/album/index.js
+++ src/album/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getOne, create, deleteOne, update } = require('./controller')
+const { getOne, create, deleteOne, update, addTrack, deleteTrack } = require('./controller')
 
 router.get('/:name', getOne);
 router.post('/', create);
 router.put('/:_id', update);
+router.put('/addtrack/:_id', addTrack);
+router.put('/deletetrack/:_id', deleteTrack);
 router.delete('/:_id', deleteOne);
 
 module.exports = router;
